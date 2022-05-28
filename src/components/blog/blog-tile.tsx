6ab08a909539ee750b561602883f67f61b336cbf
Tom Hibbers <38688272+tomhibbers@users.noreply.github.com>
2022-05-28 13:44:53 +0200

    Blog second attempt

---
+++ src/components/blog/blog-tile.tsx
@@ -0,0 +1,93 @@
+import {
+  Avatar,
+  Box,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { MotionBox } from "components/motion";
+import { CardTransition } from "components/page-transitions";
+import React from "react";
+
+const BlogTile = (props) => {
+  const { title, link, thumbnail, content, pubDate } = props.blogData;
+
+  function cleanTitle(checkTitle) {
+    checkTitle = checkTitle.replace("amp;", "");
+    return checkTitle;
+  }
+  function truncateText(text, start, len) {
+    return text.length > len ? text.slice(start, len) : text;
+  }
+  function toText(block) {
+    let tag = document.createElement("div");
+    tag.innerHTML = block;
+    block = tag.innerText;
+    return block;
+  }
+  function convertDate(date) {
+    let dateArray = date.slice(0, 10).split("-");
+    let year = dateArray.shift();
+    dateArray.push(year);
+    // console.log(date);
+    return `Published: ${dateArray.join("/")}`;
+  }
+  return (
+    <LinkBox
+      maxW={"445px"}
+      w={"full"}
+      bg={useColorModeValue("white", "gray.900")}
+      boxShadow={"2xl"}
+      rounded={"md"}
+      p={6}
+      m={6}
+      overflow={"hidden"}
+    >
+      <Box
+        height={"210px"}
+        position="relative"
+        mt={-6}
+        mx={-6}
+        mb={6}
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        backgroundImage={`url(${thumbnail})`}
+      ></Box>
+      <Stack>
+        <Text
+          color={"green.500"}
+          textTransform={"uppercase"}
+          fontWeight={800}
+          fontSize={"sm"}
+          letterSpacing={1.1}
+        >
+          Blog
+        </Text>
+        <Heading
+          color={useColorModeValue("gray.700", "white")}
+          fontSize={"2xl"}
+          fontFamily={"body"}
+        >
+          <LinkOverlay href={`${link}`}>
+            {truncateText(cleanTitle(title), 0, 60)}
+          </LinkOverlay>
+        </Heading>
+        <Text fontSize={{ base: "sm" }} textAlign={"justify"}>
+          {truncateText(toText(content), 0, 300) + "..."}
+        </Text>
+      </Stack>
+      <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+        <Stack direction={"column"} spacing={0} fontSize={"sm"}>
+          <Text color={"gray.500"}>{convertDate(pubDate)}</Text>
+        </Stack>
+      </Stack>
+    </LinkBox>
+  );
+};
+
+export default BlogTile;
--- src/components/blog/posts.tsx
+++ src/components/blog/posts.tsx
@@ -6,52 +6,80 @@ import { PageSlideFade, StaggerChildren } from "components/page-transitions";
 import Header from "components/header";
 import { MotionBox } from "components/motion";
 import axios from "axios";
+import BlogTile from "./blog-tile";
 
 const TURQUOISE = "#06b6d4";
 
 const Posts = () => {
+  const [myBlog, setMyBlog] = React.useState(null);
   const [mitem, mitemset] = React.useState([]);
   const [mprofile, mprofileset] = React.useState({
     ptitle: "",
     avatar: "",
     profileurl: "",
   });
-  const mediumUrl = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@mehulkothari05";
-  function getBlogPosts() {
-    axios
-      .get(mediumUrl)
-      .then((response) => response.data)
-      .then((data) => {
-        const avatar = data.feed.image;
-        const profileLink = data.feed.link;
-        const res = data.items; //This is an array with the content. No feed, no info about author etc..
-        const posts = res.filter((item) => item.categories.length > 0);
-        const title = data.feed.title;
-        mitemset(posts);
-        mprofileset({
-          ...mprofile,
-          ptitle: title,
-          profileurl: profileLink,
-          avatar: avatar,
-        });
-      });
-  }
+  const rss2json =
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40joshuagauthreaux";
   React.useEffect(() => {
-    getBlogPosts();
-  }, [mitem]);
+    fetch(rss2json)
+      .then((res) => res.json())
+      .then((data) => {
+        setMyBlog(data);
+        // console.log(data);
+      });
+  }, [rss2json]);
+  function displayBlogs() {
+    // console.log(myBlog)
+    return (
+      myBlog &&
+      myBlog.items &&
+      myBlog.items.map((blog) => {
+        return (
+          blog.categories.length > 0 && (
+            <BlogTile key={blog.pubDate} blogData={blog} />
+          )
+        );
+      })
+    );
+  }
+  // const mediumUrl = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@mehulkothari05";
+  // function getBlogPosts() {
+  //   axios
+  //     .get(mediumUrl)
+  //     .then((response) => response.data)
+  //     .then((data) => {
+  //       const avatar = data.feed.image;
+  //       const profileLink = data.feed.link;
+  //       const res = data.items; //This is an array with the content. No feed, no info about author etc..
+  //       const posts = res.filter((item) => item.categories.length > 0);
+  //       const title = data.feed.title;
+  //       mitemset(posts);
+  //       mprofileset({
+  //         ...mprofile,
+  //         ptitle: title,
+  //         profileurl: profileLink,
+  //         avatar: avatar,
+  //       });
+  //     });
+  // }
+  // React.useEffect(() => {
+  //   console.log("effect");
+  //   // getBlogPosts();
+  // }, [mitem]);
   return (
     <PageSlideFade>
       <Header underlineColor={TURQUOISE} mt={0} mb={0}>
         Featured Articles
       </Header>
       <StaggerChildren>
-        <Stack spacing={4} mt={12}>
+        {displayBlogs()}
+        {/* <Stack spacing={4} mt={12}>
           {mitem.map((post, index) => (
             <MotionBox whileHover={{ y: -5 }} key={index}>
               <PostCard item={post} profile={mprofile} />
             </MotionBox>
           ))}
-        </Stack>
+        </Stack> */}
       </StaggerChildren>
     </PageSlideFade>
   );
